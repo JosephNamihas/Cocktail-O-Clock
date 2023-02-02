9ed31748a274b17ab9924c51e321f937b1f73647
rbrd87 <rochellembradley@gmail.com>
2023-02-02 20:53:07 +0000

    Changed the javascript as the query wasn't carrying to the cocktail.html page

--- assets/js/cocktail.js
+++ assets/js/cocktail.js
@@ -1,22 +1,21 @@
-$(document).ready(function(){
-    var giphySearchTerm = "mojito"; // Example Search term -- Pulls from search bar
-    var apiKey = "&api_key=B1QMeeTfxi77NrOloXqbNZdThiCQkuho"
+// $(document).ready(function () {
+//     var giphySearchTerm = "mojito"; // Example Search term -- Pulls from search bar
+//     var apiKey = "&api_key=B1QMeeTfxi77NrOloXqbNZdThiCQkuho"
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    giphySearchTerm + apiKey; // Rochelle's key
-  
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response) {
-    
-        console.log(response);
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//         giphySearchTerm + apiKey; // Rochelle's key
 
-        var cocktailGifEl = $("#cocktailGif")
-        cocktailGifEl.attr("src", response.data[0].source)
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
 
-        })
-    
-        
+//         console.log(response);
+//         console.log(cocktailResponse);
 
-        });
+//         var cocktailGifEl = $("#cocktailGif")
+//         cocktailGifEl.attr("src", response.data[0].source)
+
+//     })
+
+// });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,51 @@
-var cocktailName = ""; // Example search term
-
-function getCocktail(){
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/cocktail?name=' + cocktailName,
-    headers: { 'X-Api-Key': 'oG7S2vqNUXRwDPUivFn60w==v3SxU4nBy9O504CG'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-        window.location="./cocktail.html"
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-
-});}
-
-$("#search-btn").on('click', function(){
-  cocktailName = $("#search-input").val()
-  getCocktail()
-
-
-})
-
-
-
-var giphySearchTerm = "cocktail"; // Example Search term -- Pulls from search bar
-var apiKey = "&api_key=B1QMeeTfxi77NrOloXqbNZdThiCQkuho"
-
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  giphySearchTerm + apiKey; // Rochelle's key
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-
-  console.log(response);
-
-});
-
 // Global variable to store user's search input
 
 // Make a function that will call the cocktail API using the user's input
 
 // Event listener on the search button
 
+var cocktailName = "";
+
+function getCocktail() {
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/cocktail?name=' + cocktailName,
+    headers: { 'X-Api-Key': 'oG7S2vqNUXRwDPUivFn60w==v3SxU4nBy9O504CG' },
+    contentType: 'application/json',
+    success: function (cocktailData) {
+      console.log(cocktailData);
+
+      // Todo: Look into local storage to store the users input - retrieve that on cocktail API
+    },
+    error: function ajaxError(jqXHR) {
+      console.error('Error: ', jqXHR.responseText);
+    }
+  });
+};
+
+function showCocktail() {
+  var apiKey = "&api_key=B1QMeeTfxi77NrOloXqbNZdThiCQkuho"
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    cocktailName + apiKey; // Rochelle's key
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response);
+
+    var cocktailGifEl = $("#cocktailGif")
+    cocktailGifEl.attr("src", response.data[0].source)
+
+  });
+}
+
+$("#search-btn").on('click', function (event) {
+  event.preventDefault();
+
+  cocktailName = $("#search-input").val();
+  
+  getCocktail();
+});
