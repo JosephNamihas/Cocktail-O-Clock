725e409b4533f974b5b10697ad604368896eef92
Nicola Boyle <nicola162@gmail.com>
2023-02-02 19:22:32 +0000

    added basic JS to homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,29 @@
-var name = 'mojito' // Example search term
+var cocktailName = ""; // Example search term
+
+function getCocktail(){
 $.ajax({
     method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/cocktail?name=' + name,
-    headers: { 'X-Api-Key': 'qU0ueVR5pJc3xqMqK4X4zw==tYV5AJfCLdKknVeh'},
+    url: 'https://api.api-ninjas.com/v1/cocktail?name=' + cocktailName,
+    headers: { 'X-Api-Key': 'oG7S2vqNUXRwDPUivFn60w==v3SxU4nBy9O504CG'},
     contentType: 'application/json',
     success: function(result) {
         console.log(result);
+        window.location="./cocktail.html"
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
 
-});
+});}
+
+$("#search-btn").on('click', function(){
+  cocktailName = $("#search-input").val()
+  getCocktail()
+
+
+})
+
+
 
 var giphySearchTerm = "cocktail"; // Example Search term -- Pulls from search bar
 var apiKey = "&api_key=B1QMeeTfxi77NrOloXqbNZdThiCQkuho"
@@ -27,3 +39,10 @@ $.ajax({
   console.log(response);
 
 });
+
+// Global variable to store user's search input
+
+// Make a function that will call the cocktail API using the user's input
+
+// Event listener on the search button
+
