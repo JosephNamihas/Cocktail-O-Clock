1a1505d7d07006c94778ade72116df24f6c0fa10
rbrd87 <rochellembradley@gmail.com>
2023-02-08 09:37:01 +0000

    Just cleaned up the code a little

--- assets/js/cocktail.js
+++ assets/js/cocktail.js
@@ -15,11 +15,9 @@ function retrieveCocktailImage() {
         url: cocktailURL,
         method: "GET"
     }).then(function (dbCocktail) {
-        console.log(dbCocktail)
-
         var cocktailImageEl = $("#cocktailImg");
-        cocktailImageEl.attr("src", dbCocktail.drinks[0].strDrinkThumb);
 
+        cocktailImageEl.attr("src", dbCocktail.drinks[0].strDrinkThumb);
     });
 };
 
@@ -32,27 +30,24 @@ function retrieveGifImage() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
-        // Console log to test
-        console.log('This works')
         // Random number generator to randomly select GIF
         var randomGIF = Math.floor(Math.random() * 15); // Limits to the array to the first 15 searches
 
         // Populates the Cocktail Name on the cocktail page
-        var cocktailNameEl = cocktailDataObject[0].name
-        var cocktailHeaderEl = $("#cocktail-name")
-        cocktailHeaderEl.text(cocktailNameEl)
+        var cocktailNameEl = cocktailDataObject[0].name;
+        var cocktailHeaderEl = $("#cocktail-name");
+        cocktailHeaderEl.text(cocktailNameEl);
 
         // Populates the Cocktail GIF on the cocktail page
-        var cocktailGifEl = $("#cocktailGif")
-        cocktailGifEl.attr("src", response.data[randomGIF].images.original.url)
+        var cocktailGifEl = $("#cocktailGif");
+        cocktailGifEl.attr("src", response.data[randomGIF].images.original.url);
 
         //Populates the Cocktail ingredients
         for (let i = 0; i < cocktailDataObject[0].ingredients.length; i++) {
 
             $("#ingredients").append("<li>" + cocktailDataObject[0].ingredients[i] + "</li>");
             console.log(cocktailDataObject[0].ingredients[i]);
-        }
+        };
 
         //Populates the Instructions
         $("#method").text(cocktailDataObject[0].instructions);
@@ -66,18 +61,16 @@ function retrieveGifImage() {
         $(".related-items").on('click', function (event) {
             event.preventDefault();
 
-            var cocktailName = $(this).text()
+            var cocktailName = $(this).text();
 
             localStorage.setItem("nameOfCocktail", cocktailName);
-            console.log(cocktailName)
 
-            getCocktail(cocktailName)
-        })
+            getCocktail(cocktailName);
+        });
     });
 };
 
 function loadStorage() {
-    console.log(storageArray)
     // If statement so that if the storage array is null it will not attempt to load the storage
     if (storageArray !== null) { // If the storage array has a value it will fetch it and display it
         // On page load, cycles through loop and appends everything in local storage...
@@ -88,19 +81,18 @@ function loadStorage() {
         $(".fav-items").on('click', function (event) {
             event.preventDefault();
 
-            var cocktailName = $(this).text()
+            var cocktailName = $(this).text();
 
             localStorage.setItem("nameOfCocktail", cocktailName);
-            console.log(cocktailName)
 
-            getCocktail(cocktailName)
-        })
+            getCocktail(cocktailName);
+        });
     };
 };
 
 function backToSearch() {
     window.location = './index.html';
-}
+};
 
 $(document).ready(function () {
 
@@ -111,8 +103,9 @@ $(document).ready(function () {
     // Hiding the related cocktails field if there are only 2 in the array
     if (cocktailDataObject.length <= 2) {
         $(".related").addClass("hide");
-        $(".favourites").addClass("text-center")
-    }
+
+        $(".favourites").addClass("text-center");
+    };
 });
 
 // Making function for the copy icon to copy recipe
@@ -131,7 +124,7 @@ function copyRecipe() {
     } else {
         console.log("Browser not compatible") // for checking if errors
     }
-}
+};
 
 $("#add-to-favourites").on('click', function () {
     // Validation to stop the user adding multiples of the same cocktail
@@ -141,14 +134,14 @@ $("#add-to-favourites").on('click', function () {
         //Sets index of storage array to memory
         localStorage.setItem("Favourites", JSON.stringify(storageArray));
         //Appends to page
-        $("#favourite-cocktails").append(`<li>${cocktailDataObject[0].name}</li>`)
+        $("#favourite-cocktails").append(`<li>${cocktailDataObject[0].name}</li>`);
     } else {
-        console.log("You already have this saved")
-    }
+        console.log("You already have this saved");
+    };
 });
 
 // Button to clear all favourites
 $("#delete-favourites").on('click', function () {
-    localStorage.removeItem("Favourites")
+    localStorage.removeItem("Favourites");
     location.reload();
 });
